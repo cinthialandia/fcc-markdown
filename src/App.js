2d32093b25aa31e1aad9ee8a4ef492c387d4b02c
Cinthia <cinthia.j.23@gmail.com>
2020-06-02 17:40:39 +1000

    add photo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import marked from "marked";
 import "./App.css";
 
 function App() {
   const [html, setHtml] = useState("");
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const defaultValue = `# Hello I am H1
+## Hello I am H2
+[Hello I am a link](www.cinthialandia.com)
+
+\`I am inline code\`
+
+\`\`\`
+const name = "Cinthia!"
+\`\`\`
+
+- item 1
+- item 2
+- item 3
+
+> Hello I am quote
+
+![alt text](/img/cin.jpg "Logo Title Text 1")
+
+**Hello I am bold**
+    `;
+
+    setValue(defaultValue);
+    setHtml(marked(defaultValue));
+  }, []);
 
   const handleChange = (e) => {
+    setValue(e.target.value);
     setHtml(marked(`${e.target.value}`));
     console.log(e.target.value);
   };
@@ -14,7 +42,13 @@ function App() {
     <div className="container">
       <div className="editor-container">
         <h3 className="title">Editor</h3>
-        <textarea id="editor" onChange={handleChange} type="text" name="name" />
+        <textarea
+          value={value}
+          id="editor"
+          onChange={handleChange}
+          type="text"
+          name="name"
+        />
       </div>
       <div className="previewer-container">
         <h3 className="title">Previewer</h3>
